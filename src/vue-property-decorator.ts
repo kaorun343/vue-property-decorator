aa27deb07c85a3274da358a2299bd72630e58296
kaorun343 <kaoru.h65536@live.jp>
2017-07-09 20:32:42 +0900

    update for typescript 2.4

--- src/vue-property-decorator.ts
+++ src/vue-property-decorator.ts
@@ -62,10 +62,10 @@ export function Model(event?: string): PropertyDecorator {
  * @param  options the options for the prop
  * @return PropertyDecorator | void
  */
-export function Prop(options: (PropOptions | Constructor[]) = {}): PropertyDecorator {
+export function Prop(options: (PropOptions | Constructor[] | Constructor) = {}): PropertyDecorator {
   return function (target: Vue, key: string) {
-    if (!Array.isArray(options) && typeof options.type === 'undefined') {
-      options.type = Reflect.getMetadata('design:type', target, key)
+    if (!Array.isArray(options) && typeof (options as PropOptions).type === 'undefined') {
+      (options as PropOptions).type = Reflect.getMetadata('design:type', target, key)
     }
     createDecorator((componentOptions, k) => {
       (componentOptions.props || (componentOptions.props = {}) as any)[k] = options
