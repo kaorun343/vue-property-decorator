19136ea9dcdf31e2d8fa93a0997bb6b1869bf8d8
kaorun343 <kaoru.h65536@live.jp>
2018-05-21 02:19:16 +0900

    version 6.1.0

--- src/vue-property-decorator.ts
+++ src/vue-property-decorator.ts
@@ -1,4 +1,4 @@
-/** vue-property-decorator verson 6.0.0 MIT LICENSE copyright 2018 kaorun343 */
+/** vue-property-decorator verson 6.1.0 MIT LICENSE copyright 2018 kaorun343 */
 
 'use strict'
 import Vue, { PropOptions, WatchOptions } from 'vue'
