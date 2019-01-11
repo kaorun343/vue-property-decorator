5c4c4542d244ac54f1c50c7707f85f57ce00b26e
kaorun343 <hagiharak4294967296@gmail.com>
2019-01-11 20:29:31 +0900

    Version 7.3.0
    * Update to `@Watch` decorator to support multiple watch options

--- src/vue-property-decorator.ts
+++ src/vue-property-decorator.ts
@@ -1,4 +1,4 @@
-/** vue-property-decorator verson 7.2.0 MIT LICENSE copyright 2018 kaorun343 */
+/** vue-property-decorator verson 7.3.0 MIT LICENSE copyright 2018 kaorun343 */
 
 'use strict'
 import Vue, { PropOptions, WatchOptions } from 'vue'
