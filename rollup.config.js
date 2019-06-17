1330ea428cc18720f7249ef5c90facaadc1ec4ad
kaorun343 <hagiharak4294967296@gmail.com>
2019-06-17 17:39:21 +0900

    Update packages

--- rollup.config.js
+++ rollup.config.js
@@ -1,17 +1,16 @@
 export default {
   input: 'lib/vue-property-decorator.js',
-  name: 'VuePropertyDecorator',
   output: {
     file: 'lib/vue-property-decorator.umd.js',
-    format: 'umd'
+    format: 'umd',
+    name: 'VuePropertyDecorator',
+    globals: {
+      'vue': 'Vue',
+      'vue-class-component': 'VueClassComponent'
+    },
+    exports: 'named',
   },
   external: [
     'vue', 'vue-class-component', 'reflect-metadata'
   ],
-  exports: 'named',
-  name: 'vue-property-decorator',
-  globals: {
-    'vue': 'Vue',
-    'vue-class-component': 'VueClassComponent'
-  }
 }
