c31172b3d1641368bc472d37e2f6dd24c9b52b10
Angeart <shinoyuki.angeart@gmail.com>
2019-09-02 14:58:00 +0900

    implements modelsync

--- src/vue-property-decorator.ts
+++ src/vue-property-decorator.ts
@@ -152,6 +152,38 @@ export function Model(
 }
 
 /**
+ * decorator of synced model and prop
+ * @param propName the name to interface with from outside, must be different from decorated property
+ * @param  event event name
+ * @param options options
+ * @return PropertyDecorator
+ */
+export function ModelSync(
+  propName: string,
+  event?: string,
+  options: PropOptions | Constructor[] | Constructor = {},
+) {
+  return (target: Vue, key: string) => {
+    applyMetadata(options, target, key)
+    createDecorator((componentOptions, k) => {
+      ;(componentOptions.props || ((componentOptions.props = {}) as any))[
+        propName
+      ] = options
+      componentOptions.model = { prop: propName, event: event || k }
+      ;(componentOptions.computed || (componentOptions.computed = {}))[k] = {
+        get() {
+          return (this as any)[propName]
+        },
+        set(value) {
+          // @ts-ignore
+          this.$emit(event, value)
+        },
+      }
+    })(target, key)
+  }
+}
+
+/**
  * decorator of a prop
  * @param  options the options for the prop
  * @return PropertyDecorator | void
---
+++ tests/ModelSync.spec.ts
@@ -0,0 +1,52 @@
+import Vue from 'vue'
+import 'reflect-metadata'
+import { ModelSync, Component } from '../src/vue-property-decorator'
+
+describe(ModelSync, () => {
+  const eventName = 'change'
+  const propertyName = 'checked'
+  const accessorName = 'chackedValue'
+  @Component
+  class TestComponent extends Vue {
+    @ModelSync(propertyName, eventName, Boolean) [accessorName]!: boolean
+
+    changeChecked(newValue: boolean) {
+      this[accessorName] = newValue
+    }
+  }
+
+  const initialValue = false
+  const component = new TestComponent({ propsData: { [propertyName]: initialValue } })
+  const mockFunction = jest.fn()
+  component.$emit = mockFunction
+
+  test('define model option correctly', () => {
+    expect(component.$options.model).toEqual({ prop: propertyName, event: eventName })
+  })
+
+  test('define props option correctly', () => {
+    const props = (component.$options.props as any) as Record<string, any>
+    expect(props![propertyName]).toEqual({ type: Boolean })
+  })
+
+  test('component recieves prop', () => {
+    expect(component[accessorName]).toBe(initialValue)
+  })
+
+  describe('when props has been changed', () => {
+    const newValue = true
+    component.changeChecked(newValue)
+
+    test('calls $emit method', () => {
+      expect(mockFunction).toHaveBeenCalled()
+    })
+
+    test('emits event with event name', () => {
+      expect(mockFunction.mock.calls[0][0]).toBe(eventName)
+    })
+
+    test('emits event with new value', () => {
+      expect(mockFunction.mock.calls[0][1]).toBe(newValue)
+    })
+  })
+})
