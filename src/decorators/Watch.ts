a7e7eef937ecdc89675dc0bd84e43b6140aa7d7f
kaorun343 <5625395+kaorun343@users.noreply.github.com>
2021-02-25 21:33:38 +0900

    Update Watch decorator for Vue 3

--- src/decorators/Watch.ts
+++ src/decorators/Watch.ts
@@ -1,28 +1,26 @@
 import { WatchOptions } from 'vue'
-import { createDecorator } from 'vue-class-component'
+import { createDecorator, VueDecorator } from 'vue-class-component'
 
 /**
- * decorator of a watch function
- * @param  path the path or the expression to observe
- * @param  WatchOption
- * @return MethodDecorator
+ * Decorator for watch options
+ * @param path the path or the expression to observe
+ * @param watchOptions
  */
-export function Watch(path: string, options: WatchOptions = {}) {
-  const { deep = false, immediate = false } = options
-
+export function Watch(path: string, watchOptions?: WatchOptions): VueDecorator {
   return createDecorator((componentOptions, handler) => {
-    if (typeof componentOptions.watch !== 'object') {
-      componentOptions.watch = Object.create(null)
+    if (typeof componentOptions.watch === 'undefined') {
+      type W = NonNullable<typeof componentOptions['watch']>
+      const watch = Object.create(null) as W
+      componentOptions.watch = watch
     }
 
     const watch: any = componentOptions.watch
-
     if (typeof watch[path] === 'object' && !Array.isArray(watch[path])) {
       watch[path] = [watch[path]]
     } else if (typeof watch[path] === 'undefined') {
       watch[path] = []
     }
 
-    watch[path].push({ handler, deep, immediate })
+    watch[path].push({ handler, ...watchOptions })
   })
 }
--- tests/decorators/Watch.spec.ts
+++ tests/decorators/Watch.spec.ts
@@ -1,47 +1,102 @@
-import Vue from 'vue'
-import Component from 'vue-class-component'
+import { mount, VueWrapper } from '@vue/test-utils'
+import { h } from 'vue'
+import { Vue } from 'vue-class-component'
 import { Watch } from '../../src/decorators/Watch'
 
 describe(Watch, () => {
-  const expressionA = 'EXPRESSION_A'
-  const expressionB = 'EXPRESSION_B'
+  const onChange = jest.fn()
+  const WATCH_PATH = 'WATCH_PATH'
+  const INITIAL_VALUE = 'INITIAL_VALUE'
+  const UPDATED_VALUE = 'UPDATED_VALUE'
 
-  const method1 = jest.fn()
-  const method2 = jest.fn()
+  describe('without watch options', () => {
+    class MyComponent extends Vue {
+      [WATCH_PATH] = INITIAL_VALUE
 
-  @Component
-  class Test extends Vue {
-    [expressionB] = false
+      @Watch(WATCH_PATH)
+      onChange(value: string, oldValue: string) {
+        onChange(value, oldValue)
+      }
 
-    @Watch(expressionA)
-    @Watch(expressionB, { immediate: true })
-    method1() {
-      method1()
+      @Watch(WATCH_PATH)
+      onChangeAnother() {}
+
+      render() {
+        return h('div')
+      }
     }
 
-    @Watch(expressionB, { immediate: true })
-    method2() {
-      method2()
+    let wrapper: VueWrapper<MyComponent>
+
+    beforeEach(() => {
+      wrapper = mount(MyComponent)
+    })
+
+    it('sets options correctly', () => {
+      expect(wrapper.vm.$options.watch).toMatchInlineSnapshot(`
+        Object {
+          "WATCH_PATH": Array [
+            Object {
+              "handler": "onChange",
+            },
+            Object {
+              "handler": "onChangeAnother",
+            },
+          ],
+        }
+      `)
+    })
+
+    it('does not call on mounted', () => {
+      expect(onChange).toHaveBeenCalledTimes(0)
+    })
+
+    it('calls onChange after value is changed', async () => {
+      wrapper.vm[WATCH_PATH] = UPDATED_VALUE
+      await wrapper.vm.$nextTick()
+      expect(onChange).toHaveBeenCalledTimes(1)
+    })
+
+    it('calls onChange with new value and old value', async () => {
+      wrapper.vm[WATCH_PATH] = UPDATED_VALUE
+      await wrapper.vm.$nextTick()
+      expect(onChange).toHaveBeenCalledWith(UPDATED_VALUE, INITIAL_VALUE)
+    })
+  })
+
+  describe('with watch options', () => {
+    class MyComponent extends Vue {
+      [WATCH_PATH] = INITIAL_VALUE
+
+      @Watch(WATCH_PATH, { deep: true, immediate: true, flush: 'post' })
+      onChange(value: string, oldValue: string) {
+        onChange(value, oldValue)
+      }
+
+      render() {
+        return h('div')
+      }
     }
-  }
 
-  const component = new Test()
+    let wrapper: VueWrapper<MyComponent>
 
-  beforeAll(() => {
-    component[expressionB] = true
-  })
+    beforeEach(() => {
+      wrapper = mount(MyComponent)
+    })
 
-  test('defines watch options', () => {
-    const watch = component.$options.watch as any
-    expect(watch[expressionA]).toHaveLength(1)
-    expect(watch[expressionB]).toHaveLength(2)
-
-    expect(watch[expressionA][0].handler).toBe('method1')
-    expect(watch[expressionB][1].immediate).toBe(true)
-  })
-
-  test('watch handlers are called', () => {
-    expect(method1).toHaveBeenCalled()
-    expect(method2).toHaveBeenCalled()
+    it('sets options correctly', () => {
+      expect(wrapper.vm.$options.watch).toMatchInlineSnapshot(`
+        Object {
+          "WATCH_PATH": Array [
+            Object {
+              "deep": true,
+              "flush": "post",
+              "handler": "onChange",
+              "immediate": true,
+            },
+          ],
+        }
+      `)
+    })
   })
 })
