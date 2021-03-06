f047afa2f8ab739c31cdafa65e45523272646de4
kaorun343 <5625395+kaorun343@users.noreply.github.com>
2021-03-06 19:38:12 +0900

    Remove ProvideReactive and InjectReactive decorators

--- src/index.ts
+++ src/index.ts
@@ -7,13 +7,11 @@ export { Component, Vue, mixins as Mixins }
 
 export { Emit } from './decorators/Emit'
 export { Inject } from './decorators/Inject'
-export { InjectReactive } from './decorators/InjectReactive'
 export { Model } from './decorators/Model'
 export { ModelSync } from './decorators/ModelSync'
 export { Prop } from './decorators/Prop'
 export { PropSync } from './decorators/PropSync'
 export { Provide } from './decorators/Provide'
-export { ProvideReactive } from './decorators/ProvideReactive'
 export { Ref } from './decorators/Ref'
 export { VModel } from './decorators/VModel'
 export { Watch } from './decorators/Watch'
