3d4056e9c1ce7025e37910cb2a91f90377e03468
kaorun343 <5625395+kaorun343@users.noreply.github.com>
2021-03-06 21:37:38 +0900

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,9 @@
 /** vue-property-decorator verson 9.1.2 MIT LICENSE copyright 2020 kaorun343 */
-/// <reference types='reflect-metadata'/>
-import Vue from 'vue'
-import Component, { mixins } from 'vue-class-component'
-
-export { Component, Vue, mixins as Mixins }
-
+export { mixins, Options, Vue } from 'vue-class-component'
 export { Emit } from './decorators/Emit'
 export { Inject } from './decorators/Inject'
 export { Model } from './decorators/Model'
-export { ModelSync } from './decorators/ModelSync'
 export { Prop } from './decorators/Prop'
-export { PropSync } from './decorators/PropSync'
 export { Provide } from './decorators/Provide'
 export { Ref } from './decorators/Ref'
-export { VModel } from './decorators/VModel'
 export { Watch } from './decorators/Watch'
